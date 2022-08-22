0667f94620ff428fa1832204b26366f8ce31d288
Vasko <vaskopopov@gmail.com>
2022-08-22 21:34:59 +0200

    .

---
+++ contact-app/src/components/AddContact.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class AddContact extends React.Component {
+    render() {
+        return (
+            <div className="ui main">
+                <h2>Add Contact</h2>
+                <form className="ui form">
+                    <div className="field">
+                        <label>Name</label>
+                        <input type="text" name="name" placeholder="Name" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddContact;
